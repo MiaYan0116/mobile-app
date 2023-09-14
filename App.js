8359e47496fd42b3885df9f6994555e363a9295b
Mia Yan <yan.jiaxin@northeaster.edu>
2023-09-14 15:00:20 -0700

    use props to pass values from parents to children and pass values from children to parents via callback function

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Index from './src_01_stylesheet'
+import { StyleSheet, Text, TextInput, View} from 'react-native';
+import Header from './components/Header'
+import { useState } from 'react';
+import Input from './components/Input';
 
 export default function App() {
   const name = "Mia's App";
+  const [text, setText] = useState("Hi")
+
+  const changeTextHandler = (changedText) => {
+    setText(changedText);
+  }
+
+  function changeDataHandler(data){
+    console.log(data);
+		setText(data);
+	}
+
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on {name}!</Text>
-      <Text> This is exciting</Text>
-      {/* <Index/> */}
+      {/* <Text>Open up App.js to start working on {name}!</Text> */}
+      {/* use props to pass values from parents' component to Header */}
+      <Header name={name}/>
+      <Input changedHandler={changeDataHandler}/>
+      <Text>Text is: {text}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -22,4 +37,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  input: {
+    borderColor:'blue',
+    borderBottomWidth: 2,
+    // borderWidth: "50%",
+  }
 });
---
+++ components/Header.js
@@ -0,0 +1,13 @@
+
+import {  Text, View } from 'react-native';
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <View>
+      <Text>Welcome to {props.name}</Text>
+    </View>
+  )
+}
+
+export default Header

79142fcf965ce81bfadafe353e6efd0cb9a28e63
Mia Yan <yan.jiaxin@northeaster.edu>
2023-09-14 13:36:52 -0700

    week2 first commit

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Index from './src_01_stylesheet'
 
 export default function App() {
+  const name = "Mia's App";
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on {name}!</Text>
+      <Text> This is exciting</Text>
+      {/* <Index/> */}
       <StatusBar style="auto" />
     </View>
   );

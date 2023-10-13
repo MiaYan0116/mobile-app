1015296de2c80ad2cb9cf69d3f0cfda80b09397a
Mia Yan <yan.jiaxin@northeaster.edu>
2023-10-12 22:40:09 -0700

    use headerRight to add an icon to right side of the title on Details

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { Button } from 'react-native';
 import Home from "./components/Home";
 import GoalDetails from "./components/GoalDetails";
+import PressableButton from "./components/PressableButton";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Entypo } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
-console.log(Stack);
 
 export default function App () {
   return(
@@ -15,7 +17,8 @@ export default function App () {
           headerStyle: {
             backgroundColor: '#b8a'
           },
-          headerTintColor: '#fff'
+          headerTintColor: '#fff',
+          
         }}
       >
         <Stack.Screen 
@@ -30,7 +33,17 @@ export default function App () {
           component={GoalDetails}
           options={({ route }) => {
             return {
-              title: route.params.text
+              title: route.params.text,
+              headerRight: () => {
+                return (
+                  <PressableButton
+                    defaultStyle={{backgroundColor: '#b8a', padding: 5}}
+                    pressedStyle={{opacity: 0.6}}
+                  >
+                    <Entypo name='warning' size={24} color='black'/>
+                  </PressableButton>
+                )
+              }
             }
           }}
         />
